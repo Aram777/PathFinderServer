9b0cc142e04c6b9c3d07dd2e6fa0c46540b541a9
Aram Abbasi <aramabbasi@yahoo.com>
2019-12-03 21:36:20 +0200

    New buildingtypes functions

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
+var buildingtypes = require('./routes/buildingtypes');
 var app = express();
 
 // view engine setup
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-
+app.use('/buildingtypes', buildingtypes);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
